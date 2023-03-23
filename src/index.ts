25e5863573e36fa475051dd87bbf72b8db95fdaf
VadimG <vadimku@gmail.com>
2023-03-24 00:02:46 +0200

    4

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-export const tester = (name: string) => `tester`;
+import PsGameModel from './models/PsGameModel';
+import PsGamePricesModel from "./models/PsGamePricesModel";
+import PsGameQueueModel from './models/PsGameQueueModel';
+import helpers from './utils/helpers';
+
+export {
+  PsGameModel,
+  PsGamePricesModel,
+  PsGameQueueModel,
+  helpers,
+}
